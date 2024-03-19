fa22bbe179a9c2bda06a1729e5117914c0967732
Pedro Ésli <pedroesli@users.noreply.github.com>
2024-03-18 23:19:19 -0300

    feat: acos function

---
+++ Sources/PlatoCore/Functions/Native/Math/AcosFunc.swift
@@ -0,0 +1,23 @@
+//
+//  AcosFunc.swift
+//  
+//
+//  Created by Pedro Ésli Vieira do Nascimento on 18/03/24.
+//
+
+import Darwin
+
+public struct AcosFunc: FunctionResultHandling {
+    public static let name: String = "acos"
+    
+    public static func handle(parameters: [CallParameter]) throws -> Value {
+        guard !parameters.isEmpty else { throw FunctionError.missingArgument(parameter: "x") }
+        guard parameters.count == 1 else { throw FunctionError.extraArgument }
+        
+        let x = parameters[0].value
+        
+        guard x.type.isNumber else { throw FunctionError.typeError(parameterType: x.type, expectedType: .number) }
+        
+        return Value(float: acos(x.asFloat))
+    }
+}
