294c26febe2ef4bcc03278c01989cf937077aa13
Pedro Ésli <pedroesli.vn@gmail.com>
2024-11-09 16:23:02 -0300

    Package update

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Plato",
-    platforms: [.macOS(.v11), .iOS(.v12)],
+    platforms: [.macOS(.v15), .iOS(.v18)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -37,5 +37,5 @@ let package = Package(
             ]
         )
     ],
-    swiftLanguageVersions: [.v5]
+    swiftLanguageModes: [.v5, .v6]
 )
